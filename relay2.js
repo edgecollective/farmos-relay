456fe730c9a834ec9e904f584b24d173d1db912e
Don Blair <donblair@gmail.com>
2019-06-11 04:01:47 -0400

    update

--- relay2.js
+++ relay2.js
@@ -9,10 +9,8 @@ app.use(express.static(__dirname + "/public")); //use static files in ROOT/publi
 
 const base_url='https://wolfesneck.farmos.net/farm/sensor/listener/'
 
-
-//http://localhost:1337?public_key=834c74e03901cd1702c0a3060803f767&private_key=bfe468dc77b5530d65319b67cc39cdbc&temp=12.3&moisture=30.3
-
-//https://wolfesneck.farmos.net/farm/sensor/listener/834c74e03901cd1702c0a3060803f767?private_key=bfe468dc77b5530d65319b67cc39cdbc
+// test url to use:
+//'http://localhost:1337/?public_key=834c74e03901cd1702c0a3060803f767&private_key=bfe468dc77b5530d65319b67cc39cdbc&temp=14.3&moisture=23.3'
 
 
 app.get("/", function(req, response){ //root dir
@@ -23,15 +21,10 @@ app.get("/", function(req, response){ //root dir
     console.log(req.query.private_key);
     console.log(req.query.temp);
     console.log(req.query.moisture);
-    //console.log('https://wolfesneck.farmos.net/farm/sensor/listener/'+'?public_key='+req.query.public_key+'&private_key='+req.query.private_key+'&temp='+req.query.temp+'&moisture='+req.query.moisture);
-
     //console.log(Object.keys(req.params).length);
     //console.log(req.baseUrl);
     var post_url=base_url+req.query.public_key+'?private_key='+req.query.private_key;
     console.log(post_url);
-
-//'http://localhost:1337/?public_key=834c74e03901cd1702c0a3060803f767&private_key=bfe468dc77b5530d65319b67cc39cdbc&temp=14.3&moisture=23.3'
-
 
 request.post(
     post_url,
