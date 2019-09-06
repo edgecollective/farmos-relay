55fa391af324bf19e3f1f6a443333c5f9529e24b
Don Blair <donblair@gmail.com>
2019-09-06 20:48:48 +0100

    update

--- rak_decoder.js
+++ rak_decoder.js
@@ -15,15 +15,6 @@ const base_url='https://edgecollective.farmos.net/farm/sensor/listener/'
 const public_key='d57fa2bd03e20408d17eb1d5ee58c9d6'
 const private_key='edac3534c121c8d241719a2ba853acd3'
 
-// test url to use:
-//'http://localhost:1337/?public_key=[PUBLIC_KEY]&private_key=[PRIVATE_KEY]&temp=14.3&moisture=23.3'
-
-var tempBytes = new Buffer([0x09,0x92,0xe4,0x0c])
-
-//console.log(decoder.temperature(tempBytes.slice(0,0+2)))
-
-console.log(decoder.decode(tempBytes,[decoder.temperature,decoder.humidity],['temp','humidiy']));
-
 app.post("/", function(req,response){
 	response.status(200).send(req.body);
 	//console.log(req.body.temp);
@@ -35,27 +26,21 @@ app.post("/", function(req,response){
 
     //console.log(req.body.object.DecodeDataString)
 
-    var myvar = req.body.object.DecodeDataString
-    var myvar = "0x09,0x92,0x00";
-    console.log(decoder.temperature(bytes.slice(x,x+2)))
+    var hex = req.body.object.DecodeDataHex.split(',')
+    console.log(hex)
+    var tempBytes = new Buffer(hex)
+    var params = decoder.decode(tempBytes,[decoder.temperature,decoder.humidity],['temp','humidity']);
 
 
-    var tempBytes = new Buffer([0x09,0x92,0xe4,0x0c])
-
-//console.log(decoder.temperature(tempBytes.slice(0,0+2)))
-
-console.log(decoder.decode(tempBytes,[decoder.temperature,decoder.humidity],['temp','humidiy']));
-
-
-
+    console.log(params)
 	//var myvar = String('hello');
 
-	console.log(myvar);
+	//console.log(myvar);
 
 	request.post(
     post_url,
     //{ json: { 'temp': 23.,'moisture':32. } },
-    { json: { 'temp': 18, 'moisture':myvar } },  
+    { json: { 'temp': params.temp, 'humidity':params.humidity } },  
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
